9a8bde649b7040f0d977abfa51bc40c3fa93d936
Cheese <36419194+Cheesegle@users.noreply.github.com>
2022-10-15 17:48:33 +0000

    integrate api

--- index.js
+++ index.js
@@ -5,37 +5,27 @@ import {
   GatewayIntentBits,
   EmbedBuilder,
   SlashCommandBuilder
-} from 'discord.js'
+} from 'discord.js';
 
-import fs from 'fs'
+import fs from 'fs';
+
+import fetch from 'node-fetch';
 
 import {
   JsonDB,
   Config
-} from 'node-json-db'
+} from 'node-json-db';
 var db = new JsonDB(new Config("galaxy", true, false, '/'))
 
-import prettyMilliseconds from 'pretty-ms'
+import prettyMilliseconds from 'pretty-ms';
 
 let TOKEN = process.env['token']
 let CLIENT_ID = '850950666020061234'
 let channelId = '1015199101194870884'
 let roleId = '1030193259009544333'
-let regentime = 1.44e+7
+let regentime = 1.44e+7;
 
 //(4 hours) 1.44e+7 
-
-const commandadd = new SlashCommandBuilder()
-  .setName('add')
-  .setDescription('Add an enemy')
-  .addStringOption(option =>
-    option.setName('name')
-      .setDescription('Enemy username')
-      .setRequired(true))
-  .addBooleanOption(option =>
-    option.setName('dead')
-      .setDescription('Main planet dead')
-      .setRequired(true))
 
 const commandcolony = new SlashCommandBuilder()
   .setName('colony')
@@ -57,6 +47,10 @@ const commandcolony = new SlashCommandBuilder()
     option.setName('dead')
       .setDescription('Colony dead')
       .setRequired(true))
+  .addStringOption(option =>
+    option.setName('note')
+      .setDescription('Note')
+      .setRequired(false))
 
 const commandkill = new SlashCommandBuilder()
   .setName('dead')
@@ -74,15 +68,6 @@ const commandkill = new SlashCommandBuilder()
     option.setName('dead')
       .setDescription('Planet dead state')
       .setRequired(true))
-
-const commandremove = new SlashCommandBuilder()
-  .setName('remove')
-  .setDescription('Remove enemy')
-  .addStringOption(option =>
-    option.setName('name')
-      .setDescription('Enemy username')
-      .setRequired(true)
-      .setAutocomplete(true))
 
 const commandremovecolony = new SlashCommandBuilder()
   .setName('removecolony')
@@ -114,18 +99,47 @@ const nuke = new SlashCommandBuilder()
       .setDescription('Are you sure? (YES)')
       .setRequired(true))
 
+const commandautofill = new SlashCommandBuilder()
+  .setName('autofill')
+  .setDescription('Auto fill clan users')
+  .addStringOption(option =>
+    option.setName('name')
+      .setDescription('Clan username')
+      .setRequired(true))
+
+const commandnote = new SlashCommandBuilder()
+  .setName('note')
+  .setDescription('Add/edit planet note')
+  .addStringOption(option =>
+    option.setName('name')
+      .setDescription('Enemy username')
+      .setRequired(true)
+      .setAutocomplete(true))
+  .addIntegerOption(option =>
+    option.setName('planet')
+      .setDescription('Planet #')
+      .setRequired(true))
+  .addStringOption(option =>
+    option.setName('note')
+      .setDescription('Note')
+      .setRequired(true))
+
 const commands = [{
   name: 'all',
   description: 'Lists all enemies',
 },
-  commandadd,
+{
+  name: 'attackable',
+  description: 'Lists all attackable enemies',
+},
   commandcolony,
   commandkill,
-  commandremove,
   commandremovecolony,
   enemy,
-  nuke
-]
+  nuke,
+  commandautofill,
+  commandnote
+];
 
 const rest = new REST({
   version: '10'
@@ -143,11 +157,20 @@ const rest = new REST({
   } catch (error) {
     console.error(error)
   }
-})()
+})();
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
-})
+});
+
+// let response = await fetch(`https://api.galaxylifegame.net/users/name?name=${'3Cheese'}`)
+// let data = await response.json()
+// console.log(data.Online)
+
+// response = await fetch(`https://api.galaxylifegame.net/alliances/get?name=${'furry femboy club'}`)
+// data = await response.json()
+// console.log(data.Members)
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
@@ -155,35 +178,61 @@ client.on('ready', () => {
     try {
       let data = await db.getData(`/players`)
       for (let name in data) {
+        let pingattackable = 0
         let entry = data[name]
+        let memberResponse = await fetch(`https://api.galaxylifegame.net/users/name?name=${name}`)
+        let memberData = await memberResponse.json()
+        entry.online = memberData.Online
+        db.push(`/players/${name}`, entry)
         for (let [index, planet] of entry.planets.entries()) {
-          if (planet.reload <= Date.now() && planet.reload !== 0) {
-            entry.planets[index] = {
-              x: planet.x,
-              y: planet.y,
-              dead: false,
-              reload: 0
-            }
+          if (entry.online === true) {
+            entry.planets[index].dead = false
+            entry.planets[index].reload = -1
             db.push(`/players/${name}`, entry)
-            let channel = await client.channels.fetch(channelId)
-            //<@&' + roleId + '> 
-            channel.send('Planet regenerated!');
-            channel.send({
-              embeds: [await playerembed(name)]
-            });
           }
+          if (entry.online === false && planet.reload === -1) {
+            entry.planets[index].dead = false
+            entry.planets[index].reload = 0
+            db.push(`/players/${name}`, entry)
+            pingattackable += 1
+          }
+          if (planet.reload <= Date.now() && planet.reload > 0) {
+            entry.planets[index].dead = false
+            entry.planets[index].reload = 0
+            db.push(`/players/${name}`, entry)
+            pingattackable += 1
+          }
+        }
+        if (pingattackable === 1) {
+          let channel = await client.channels.fetch(channelId)
+          //<@&' + roleId + '> 
+          channel.send('Planet attackable!');
+          channel.send({
+            embeds: [await playerembed(name)]
+          });
+        } else if (pingattackable > 1) {
+          let channel = await client.channels.fetch(channelId)
+          //<@&' + roleId + '> 
+          channel.send('Planets attackable!');
+          channel.send({
+            embeds: [await playerembed(name)]
+          });
         }
       }
     } catch (error) {
 
     }
   }, 1000)
-})
+});
 
 async function playerembed(name) {
   let entry = await db.getData(`/players/${name}`)
+  let sonline = '(OFFLINE)'
+  if (entry.online === true) {
+    sonline = `(ONLINE)`
+  }
   let embed = new EmbedBuilder()
-    .setTitle(name)
+    .setTitle(`${name} ${sonline}`)
   let deadc = 0
   let deadcolor = false
   for (let [index, planet] of entry.planets.entries()) {
@@ -192,21 +241,25 @@ async function playerembed(name) {
       deadc++
       sdead = `(DEAD) (${prettyMilliseconds(planet.reload - Date.now())})`
     }
+    let snote = ''
+    if (planet.note) {
+      snote = ` (${planet.note})`
+    }
     if (index === 0) {
       embed.addFields({
         name: `Main #0`,
-        value: `${sdead}`
+        value: `${sdead}${snote}`
       })
     } else {
       embed.addFields({
         name: `Colony #${index}`,
-        value: `${planet.x}, ${planet.y} ${sdead}`,
+        value: `${planet.x}, ${planet.y} ${sdead}${snote}`,
       })
     }
   }
-  if (deadc === entry.planets.length) {
+  if (entry.online === true) {
     embed.setColor(0xff1100)
-  } else if (deadc === 0) {
+  } else if (deadc < entry.planets.length) {
     embed.setColor(0x00ff00)
   } else {
     embed.setColor(0xffff00)
@@ -217,11 +270,14 @@ async function playerembed(name) {
 client.on('interactionCreate', async interaction => {
   if (!interaction.isAutocomplete()) return
   if (interaction.channel.id !== '1015199101194870884') return
-  let focusedValue = interaction.options.getFocused();
+  let focusedValue = interaction.options.getFocused().toLowerCase();
   let choices = Object.keys(await db.getData(`/players`))
-  let filtered = choices.filter(choice => choice.startsWith(focusedValue));
+  let filtered = choices.filter(choice => choice.toLowerCase().startsWith(focusedValue));
   await interaction.respond(
-    filtered.map(choice => ({ name: choice, value: choice })),
+    filtered.map(choice => ({
+      name: choice,
+      value: choice
+    })),
   );
 })
 
@@ -256,29 +312,12 @@ client.on('interactionCreate', async interaction => {
       return
     }
   }
-  if (interaction.commandName === 'add') {
-    let name = interaction.options.getString('name')
-    let dead = interaction.options.getBoolean('dead')
-    let reload = 0
-    if (dead === true) {
-      reload = Date.now() + regentime
-    }
-    db.push(`/players/${name}`, {
-      planets: [{
-        dead: dead,
-        reload: reload
-      }]
-    })
-    await interaction.reply('New enemy added!')
-    await interaction.followUp({
-      embeds: [await playerembed(name)]
-    })
-  }
   if (interaction.commandName === 'colony') {
     let name = interaction.options.getString('name')
     let x = interaction.options.getInteger('x')
     let y = interaction.options.getInteger('y')
     let dead = interaction.options.getBoolean('dead')
+    let note = interaction.options.getString('note')
     let reload = 0
     if (dead === true) {
       reload = Date.now() + regentime
@@ -289,7 +328,8 @@ client.on('interactionCreate', async interaction => {
         x: x,
         y: y,
         dead: dead,
-        reload: reload
+        reload: reload,
+        note: note
       })
       db.push(`/players/${name}`, data)
       await interaction.reply('New colony added!')
@@ -322,15 +362,6 @@ client.on('interactionCreate', async interaction => {
       } else {
         await interaction.reply('Planet does not exist!')
       }
-    } catch (error) {
-      await interaction.reply('Enemy does not exist!')
-    }
-  }
-  if (interaction.commandName === 'remove') {
-    let name = interaction.options.getString('name')
-    try {
-      await db.delete(`/players/${name}`)
-      await interaction.reply('Removed enemy!')
     } catch (error) {
       await interaction.reply('Enemy does not exist!')
     }
@@ -375,6 +406,104 @@ client.on('interactionCreate', async interaction => {
       });
       await db.delete(`/players`)
       await interaction.reply('Enemy list cleared! (backup was saved on server side)')
+    }
+  }
+  if (interaction.commandName === 'autofill') {
+    try {
+      let clanResponse = await fetch(`https://api.galaxylifegame.net/alliances/get?name=${'furry femboy club'}`)
+      let clanData = await clanResponse.json()
+      await interaction.reply('Adding members...')
+      for (let member of clanData.Members) {
+        let memberResponse = await fetch(`https://api.galaxylifegame.net/users/name?name=${member.Name}`)
+        let memberData = await memberResponse.json()
+        db.push(`/players/${member.Name}`, {
+          online: memberData.Online,
+          planets: [{
+            dead: false,
+            reload: 0
+          }]
+        })
+      }
+      let data = await db.getData(`/players`)
+      let embeds = []
+      for (let name in data) {
+        embeds.push(await playerembed(name))
+      }
+
+      await interaction.followUp('Clan members added!')
+
+      let chunkSize = 10;
+      for (let i = 0; i < embeds.length; i += chunkSize) {
+        let chunk = embeds.slice(i, i + chunkSize);
+        if (i < chunkSize) {
+          await interaction.followUp({
+            embeds: chunk
+          })
+        } else {
+          await interaction.followUp({
+            embeds: chunk
+          })
+        }
+      }
+    } catch (error) {
+      await interaction.reply('Unable to fetch clan!')
+    }
+  }
+  if (interaction.commandName === 'attackable') {
+    try {
+      let data = await db.getData(`/players`)
+      let embeds = []
+      for (let name in data) {
+        let isaplanetalive = false
+        let entry = data[name]
+        if (entry.online === false) {
+          for (let planet of entry.planets) {
+            if (planet.dead === false) {
+              isaplanetalive = true
+            }
+          }
+        }
+        if (isaplanetalive === true) {
+          embeds.push(await playerembed(name))
+        }
+      }
+
+      let chunkSize = 10;
+      for (let i = 0; i < embeds.length; i += chunkSize) {
+        let chunk = embeds.slice(i, i + chunkSize);
+        if (i < chunkSize) {
+          await interaction.reply({
+            embeds: chunk
+          })
+        } else {
+          await interaction.followUp({
+            embeds: chunk
+          })
+        }
+      }
+    } catch (error) {
+      await interaction.reply('No enemies!')
+      return
+    }
+  }
+  if (interaction.commandName === 'note') {
+    let name = interaction.options.getString('name')
+    let planet = interaction.options.getInteger('planet')
+    let note = interaction.options.getString('note')
+    try {
+      let data = await db.getData(`/players/${name}`)
+      if (data.planets[planet]) {
+        data.planets[planet].note = note
+        db.push(`/players/${name}`, data)
+        await interaction.reply('Set planet note!')
+        await interaction.followUp({
+          embeds: [await playerembed(name)]
+        })
+      } else {
+        await interaction.reply('Planet does not exist!')
+      }
+    } catch (error) {
+      await interaction.reply('Enemy does not exist!')
     }
   }
 })
